4eb67d7013bc385f99123cc96645345ccfabb4ea
Svend Sorensen <ssorensen@whitepages.com>
2015-02-23 15:38:32 -0800

    Check traffic IP group IP addresses against valid networks

--- resource_traffic_ip_group.go
+++ resource_traffic_ip_group.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net"
 
 	"github.com/whitepages/terraform-provider-stingray/Godeps/_workspace/src/github.com/hashicorp/terraform/helper/hashcode"
 	"github.com/whitepages/terraform-provider-stingray/Godeps/_workspace/src/github.com/hashicorp/terraform/helper/schema"
@@ -170,6 +171,17 @@ func resourceTrafficIPGroupSet(d *schema.ResourceData, meta interface{}) error {
 	setString(&r.Basic.Multicast, d, "multicast")
 	setString(&r.Basic.Note, d, "note")
 	setStringSet(&r.Basic.Slaves, d, "slaves")
+
+	ns := meta.(*providerConfig).validNetworks
+	for _, s := range *r.Basic.IPAddresses {
+		ip := net.ParseIP(s)
+		if ip == nil {
+			return fmt.Errorf("Invalid IP address: %s", s)
+		}
+		if !ns.Contains(ip) {
+			return fmt.Errorf("IP address %s is not in the valid networks", ip)
+		}
+	}
 
 	_, err := c.Set(r)
 	if err != nil {
